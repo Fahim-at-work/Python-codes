67d6c32a5ac64dfd0231844d68b2233a758aea96
Fahim Shariff <52634330+Fahim-at-work@users.noreply.github.com>
2020-03-25 23:21:19 -0400

    added tip calculator

---
+++ tip.py
@@ -0,0 +1,31 @@
+def TakeInput():
+    try:
+        total_bill = float(input("Enter the total bill amount: "))
+        number_of_people = int(input("Enter the number of people you want to split the bill: "))
+    except ValueError as err:
+        print(err)
+    return total_bill, number_of_people
+
+
+def CalculatorTip(x, y):
+    """
+    :param y: int
+    :type x: float
+    """
+    tip_amount = 0.13 * x
+    individual_tip = tip_amount / y
+    return tip_amount, individual_tip
+
+
+def ShowOutput(d, f):
+    print("Total tip {0:.2f}".format(d))
+    print("Individual tip {0:.2f}".format(f))
+
+
+def main():
+    a, b = TakeInput()
+    c, d = CalculatorTip(a, b)
+    ShowOutput(c, d)
+
+
+main()
